a340a0f8457727685ff268157765bf5d0bc67018
AhCamargo <andre.camargo500@gmail.com>
2024-10-14 10:46:02 -0300

    Deploy 14 October - 2024

--- build/database/migrations/index.js
+++ build/database/migrations/index.js
@@ -1,20 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.migrationDir = void 0;
-var _1694720577496_createUsers_1 = require("./1694720577496-createUsers");
-var _1697084968105_permissions_1 = require("./1697084968105-permissions");
-var _1720832053181_AddClientNew_1 = require("./1720832053181-AddClientNew");
-var _1721239748864_AddUniqueConstraintToCodeClient_1 = require("./1721239748864-AddUniqueConstraintToCodeClient");
-var _1720831489814_AddClientContactsRelation_1 = require("./1720831489814-AddClientContactsRelation");
-var _1716314850943_Notifications_1 = require("./1716314850943-Notifications");
 var _1724183698162_GrainContract_1 = require("./1724183698162-GrainContract");
 exports.migrationDir = {
-    CreateUsers: _1694720577496_createUsers_1.CreateUsers1694720577496,
-    CreatePermissions: _1697084968105_permissions_1.Permissions1697084968105,
-    AddClientNew: _1720832053181_AddClientNew_1.AddClientNew1720832053181,
-    AddUniqueConstraintToCodeClient: _1721239748864_AddUniqueConstraintToCodeClient_1.AddUniqueConstraintToCodeClient1721239748864,
-    AddClientContactsRelation: _1720831489814_AddClientContactsRelation_1.AddClientContactsRelation1720831489814,
-    Notifications: _1716314850943_Notifications_1.Notifications1716314850943,
+    // CreateUsers,
+    // CreatePermissions,
+    // AddClientNew,
+    // AddUniqueConstraintToCodeClient,
+    // AddClientContactsRelation,
+    // Notifications,
     GrainContract: _1724183698162_GrainContract_1.GrainContract1724183698162,
 };
 //# sourceMappingURL=index.js.map
