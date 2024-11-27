31c59bb40078111f5826ead2d4eef550a811db7a
AhCamargo <andre.camargo500@gmail.com>
2024-11-27 12:10:28 -0300

    Add new field called complement_destination in grain_contracts table

--- build/database/migrations/index.js
+++ build/database/migrations/index.js
@@ -7,6 +7,7 @@ var _1720832053181_AddClientNew_1 = require("./1720832053181-AddClientNew");
 var _1720831489814_AddClientContactsRelation_1 = require("./1720831489814-AddClientContactsRelation");
 var _1716314850943_Notifications_1 = require("./1716314850943-Notifications");
 var _1724183698162_GrainContract_1 = require("./1724183698162-GrainContract");
+var _1732715196786_AddComplementationDestination_1 = require("./1732715196786-AddComplementationDestination");
 exports.migrationDir = {
     CreatePermissions: _1697084968105_permissions_1.Permissions1697084968105,
     CreateUsers: _1694720577496_createUsers_1.CreateUsers1694720577496,
@@ -14,5 +15,6 @@ exports.migrationDir = {
     AddClientContactsRelation: _1720831489814_AddClientContactsRelation_1.AddClientContactsRelation1720831489814,
     Notifications: _1716314850943_Notifications_1.Notifications1716314850943,
     GrainContract: _1724183698162_GrainContract_1.GrainContract1724183698162,
+    AddComplementationDestination: _1732715196786_AddComplementationDestination_1.AddComplementationDestination1732715196786,
 };
 //# sourceMappingURL=index.js.map
